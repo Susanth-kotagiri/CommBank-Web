793eed99f90655fe565b04ec13d0419b2ebab504
Susanth-kotagiri <kotagirisaisusanth@gmail.com>
2026-07-01 12:38:10 +0530

    Implement emoji picker for goal updates
    Added emoji selection functionality to update goal.

--- src/components/EmojiPicker/EmojiPicker.ts
+++ src/components/EmojiPicker/EmojiPicker.ts
@@ -18,3 +18,22 @@ export default function EmojiPicker(props: Props) {
     />
   )
 }
+
+const pickEmojiOnClick = (emoji: BaseEmoji, event: MouseEvent) => {
+  event.stopPropagation()
+
+  setIcon(emoji.native)
+  setEmojiPickerIsOpen(false)
+
+  const updatedGoal: Goal = {
+    ...props.goal,
+    icon: emoji.native ?? props.goal.icon,
+    name: name ?? props.goal.name,
+    targetDate: targetDate ?? props.goal.targetDate,
+    targetAmount: targetAmount ?? props.goal.targetAmount,
+  }
+
+  dispatch(updateGoalRedux(updatedGoal))
+
+  // TODO(TASK-3) Update database
+}
