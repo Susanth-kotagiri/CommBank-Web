aebff0f3568980392c501c8c87985fd9b3ad45cd
Susanth-kotagiri <kotagirisaisusanth@gmail.com>
2026-07-01 12:55:07 +0530

    Update goalmanager.tsx

--- src/components/EmojiPicker/goalmanager.tsx
+++ src/components/EmojiPicker/goalmanager.tsx
@@ -37,3 +37,32 @@ export function GoalManager(props: Props) {
     {/* ... */}
   )
 }
+// GoalManager.tsx
+
+export function GoalManager(props: Props) {
+  // ...
+
+  const [icon, setIcon] = useState<string | null>(null)
+
+  useEffect(() => {
+    setIcon(props.goal.icon)
+  }, [props.goal.id, props.goal.icon])
+
+  const hasIcon = () => icon != null
+
+  const addIconOnClick = (event: React.MouseEvent) => {
+    event.stopPropagation()
+    setEmojiPickerIsOpen(true)
+  }
+
+  return (
+    {/* ... */}
+    <AddIconButtonContainer hasIcon={hasIcon()}>
+      <TransparentButton onClick={addIconOnClick}>
+        <FontAwesomeIcon icon={faSmile} size="2x" />
+        <AddIconButtonText>Add icon</AddIconButtonText>
+      </TransparentButton>
+    </AddIconButtonContainer>
+    {/* ... */}
+  )
+}
